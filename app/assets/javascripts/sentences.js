3aa28b00d0b455a4dceab8d45d3863d7307ff30d
robertparker <rohit.parulkar@gmail.com>
2013-02-05 23:28:35 -0500

    GetSentence now appends text to empty element, rather than showing it

--- app/assets/javascripts/sentences.js
+++ app/assets/javascripts/sentences.js
@@ -4,13 +4,13 @@ $(document).ready(function() {
 	$randindex = 0
 	$counter = 0  //gotta be a better way to do this. 
 
-// add text in $randindex to first div, then.
 
 	$( "#the-button" ).click(function( event ) {
 		event.preventDefault();
 		$counter = 0
 		getRandNum()
-		showSentence($randindex);
+		removeSentence()
+		addSentence($randindex)
 		console.log('randgroup is '+ $randgroup + ', randinindex is '+$randindex);
 	});
 
@@ -19,51 +19,53 @@ $(document).ready(function() {
 		event.preventDefault();
 		showGrouping($randgroup)
 		console.log('context: randgroup is '+$randgroup)
-
 	});
 
 
 
-	function showSentence(number){
-		$(".sentence").hide()
-		$(".sentence[data-index=" + number + "]").show()
-		// console.log($(".sentence[data-index=" + number + "]").attr('data-source'))
-		
-	};
-
-		
-	
-	function showGrouping($randgroup){
-		if($counter == 0) {
-				$counter ++
+		function showSentence(number){
 			$(".sentence").hide()
-	 		// $(".sentence[data-grouping=" + $randgroup + "]").show()
-			$(".sentence[data-index=" + $randindex + "]").append($(".sentence[data-index=" + ($randindex + 1) + "]").text())
-			$(".sentence[data-index=" + $randindex + "]").prepend($(".sentence[data-index=" + ($randindex - 1) + "]").text())
-			$(".sentence[data-index=" + $randindex + "]").show()
+			$(".sentence[data-index=" + number + "]").show()			
+		};
+
+		function addSentence(number) {
+			$("#sentenceview").append("<span>" + $(".sentence[data-index=" + number + "]").text() + "</span>").show()
 		}
-		else {
 
+		function removeSentence() {
+			$("#sentenceview").contents().remove()
 		}
-	// var block = $(".sentence[data-index=" + $randindex + "]");
-	// 	block.css("height", block.height());
-	//     block.children().append($(".sentence[data-index=" + ($randindex + 1) + "]");
- //    block.animate({
- //        height: block.children().height()
- //    })
-    // http://stackoverflow.com/questions/10565832/div-append-text-with-an-animation is how to do this.
-	
-	};
+
+		function showGrouping($randgroup){
+			if($counter == 0) {
+					$counter ++
+				$("#sentenceview").hide()
+				$("#sentenceview").append($(".sentence[data-index=" + ($randindex + 1) + "]").text())
+				$("#sentenceview").prepend($(".sentence[data-index=" + ($randindex - 1) + "]").text())
+				$("#sentenceview").show().slideDown()
 
 
 
-	function getRandNum() {
-			$randgroup = Math.floor(Math.random()*50)
-			$randindex = 3*$randgroup-1;
-			
-	};
+			}
+			else {
+
+			}
+		
+	    // http://stackoverflow.com/questions/10565832/div-append-text-with-an-animation is how to do this.
+			// 			var block = $("#sentenceview");
+			// block.css("height", block.height());
+			// 	    block.animate({
+	  //       height: block.children().height()})
+		};
+
+
+
+		function getRandNum() {
+				$randgroup = Math.ceil(Math.random()*50)
+				$randindex = 3*$randgroup-1;
+				
+		};
 
 
 });
-
 
