aed43bad93c7acd7316adb603f264ecfed687cef
robertparker <rohit.parulkar@gmail.com>
2013-02-17 21:30:35 -0500

    highlights the get one button to click

--- app/assets/javascripts/sentences.js
+++ app/assets/javascripts/sentences.js
@@ -2,16 +2,18 @@ $(document).ready(function() {
 
 	$randgroup = 0
 	$randindex = 0
-	$counter = 0  //gotta be a better way to do this. 
+	$counter = 0  //prevents the grouping from being added twice.  
 
 
 	$( "#the-button" ).click(function( event ) {
 		event.preventDefault();
-		$counter = 0
+		$("#the-button").removeClass('activebutton') //removes the gold-border highlight
+		$counter = 0 
 		getRandNum()
 		removeSentence()
 		addSentence($randindex)
 		console.log('randgroup is '+ $randgroup + ', randinindex is '+$randindex);
+
 	});
 
 
@@ -19,8 +21,10 @@ $(document).ready(function() {
 		event.preventDefault();
 		showGrouping($randgroup)
 		console.log('context: randgroup is '+$randgroup)
+		makeButtonActive()
 	});
 
+		//adds a sentence into the span within the sentenceview element. Span is used to differentiate sentences by color.
 		function addSentence(number) {
 			var item = $("#sentenceview").append("<span>" + $(".sentence[data-index=" + number + "]").text() + "</span>").hide().fadeIn(200)
 		}
@@ -29,6 +33,7 @@ $(document).ready(function() {
 			$("#sentenceview").contents().remove()
 		}
 
+		//adds the sentence's grouping (sentence before and after) into #sentenceview
 		function showGrouping($randgroup){
 			if($counter == 0) {
 					$counter ++
@@ -42,10 +47,7 @@ $(document).ready(function() {
 				}
 				$("#sentenceview").show().slideDown()
 			}
-			else {
 
-			}
-		
 	    // http://stackoverflow.com/questions/10565832/div-append-text-with-an-animation is how to do this.
 			// 			var block = $("#sentenceview");
 			// block.css("height", block.height());
@@ -53,13 +55,15 @@ $(document).ready(function() {
 	  //       height: block.children().height()})
 		};
 
+		function makeButtonActive() {
+			$("#the-button").addClass('activebutton')
+		};
 
-
+		//generates the middle sentence (randindex) of any random grouping (randgroup)
 		function getRandNum() {
 				$randgroup = Math.ceil(Math.random()*$('.sentence:last').attr('data-grouping'))
 				$randindex = 3*$randgroup-1;
-				console.log('length is '+$('.sentence:last').attr('data-grouping'))
-				
+				// console.log('length is '+$('.sentence:last').attr('data-grouping'))				
 		};
 
 
